3ed8de01d9a918cb08d51bb22724f2e666457d7f
ljt990218 <2511590093@qq.com>
2024-09-09 15:52:25 +0800

    refactor(pages): some page `index profile`

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-// import type { PickerColumn } from 'vant'
 import { closeToast, showLoadingToast } from 'vant'
-// import useAppStore from '@/stores/modules/app'
-// import { languageColumns, locale } from '@/utils/i18n'
 import { queryTodoList } from '@/api'
 
 definePage({
@@ -16,33 +13,6 @@ showLoadingToast({
   message: 'load...',
   forbidClick: true,
 })
-
-// const appStore = useAppStore()
-const checked = ref<boolean>(isDark.value)
-
-watch(
-  () => isDark.value,
-  (newMode) => {
-    checked.value = newMode
-  },
-  { immediate: true },
-)
-
-// function toggle() {
-//   toggleDark()
-//   appStore.switchMode(isDark.value ? 'dark' : 'light')
-// }
-
-// const { t } = useI18n()
-
-// const showLanguagePicker = ref(false)
-// const languageValues = ref<Array<string>>([locale.value])
-// const language = computed(() => languageColumns.find(l => l.value === locale.value).text)
-
-// function onLanguageConfirm(event: { selectedOptions: PickerColumn }) {
-//   locale.value = event.selectedOptions[0].value as string
-//   showLanguagePicker.value = false
-// }
 
 const todoList = ref([])
 queryTodoList().then(({ code, data }) => {
@@ -61,35 +31,17 @@ queryTodoList().then(({ code, data }) => {
 <template>
   <div class="pt-46">
     <div class="px-16">
-      <div v-for="item in todoList" :key="item.id" class="mt-10 rounded-8 bg-white py-16 pl-8">
+      <div v-for="item in todoList" :key="item.id" class="todo mt-10 rounded-8 py-16 pl-8">
         <van-checkbox v-model="item.checked">
           {{ item.todo }}
         </van-checkbox>
       </div>
     </div>
-
-    <!-- <VanCellGroup inset>
-      <VanCell center :title="t('menus.darkMode')">
-        <template #right-icon>
-          <VanSwitch v-model="checked" size="20px" aria-label="on/off Dark Mode" @click="toggle()" />
-        </template>
-      </VanCell>
-
-      <VanCell
-        is-link
-        :title="t('menus.language')"
-        :value="language"
-        @click="showLanguagePicker = true"
-      />
-
-      <van-popup v-model:show="showLanguagePicker" position="bottom">
-        <van-picker
-          v-model="languageValues"
-          :columns="languageColumns"
-          @confirm="onLanguageConfirm"
-          @cancel="showLanguagePicker = false"
-        />
-      </van-popup>
-    </VanCellGroup> -->
   </div>
 </template>
+
+<style lang="less" scoped>
+.todo {
+  background: var(--van-cell-background);
+}
+</style>
--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import type { PickerColumn } from 'vant'
 import { useUserStore } from '@/stores'
 import { formatDate } from '@/utils/date'
+import useAppStore from '@/stores/modules/app'
+import { languageColumns, locale } from '@/utils/i18n'
 
 definePage({
   name: 'profile',
@@ -12,14 +15,52 @@ definePage({
 })
 
 const { userInfo } = useUserStore()
+const { t } = useI18n()
+const appStore = useAppStore()
+const checked = ref<boolean>(isDark.value)
 
-// const { t } = useI18n()
+function toggle() {
+  toggleDark()
+  appStore.switchMode(isDark.value ? 'dark' : 'light')
+}
+
+const showLanguagePicker = ref(false)
+const languageValues = ref<Array<string>>([locale.value])
+const language = computed(() => languageColumns.find(l => l.value === locale.value).text)
+
+function onLanguageConfirm(event: { selectedOptions: PickerColumn }) {
+  locale.value = event.selectedOptions[0].value as string
+  showLanguagePicker.value = false
+}
 </script>
 
 <template>
-  <div class="mx-16 mt-66 overflow-hidden rounded-8">
-    <van-cell title="Name" :value="userInfo.name" />
-    <van-cell title="Age" :value="userInfo.age" />
-    <van-cell title="CreateDate" :value="formatDate(userInfo.createDate)" />
+  <div class="mt-66 overflow-hidden rounded-8">
+    <VanCellGroup inset>
+      <van-cell title="Name" :value="userInfo.name" />
+      <van-cell title="Age" :value="userInfo.age" />
+      <van-cell title="CreateDate" :value="formatDate(userInfo.createDate)" />
+      <VanCell center :title="t('menus.darkMode')">
+        <template #right-icon>
+          <VanSwitch v-model="checked" size="20px" aria-label="on/off Dark Mode" @click="toggle()" />
+        </template>
+      </VanCell>
+
+      <VanCell
+        is-link
+        :title="t('menus.language')"
+        :value="language"
+        @click="showLanguagePicker = true"
+      />
+    </VanCellGroup>
   </div>
+
+  <van-popup v-model:show="showLanguagePicker" position="bottom">
+    <van-picker
+      v-model="languageValues"
+      :columns="languageColumns"
+      @confirm="onLanguageConfirm"
+      @cancel="showLanguagePicker = false"
+    />
+  </van-popup>
 </template>
